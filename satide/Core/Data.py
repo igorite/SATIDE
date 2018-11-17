52d26bc85b6573df1875f358f72c5244c87ad365
socisomer <41864144+socisomer@users.noreply.github.com>
2018-11-17 07:41:53 +0100

    Create Data.py

---
+++ satide/Core/Data.py
@@ -0,0 +1,13 @@
+
+links = []
+
+
+def remove_links_by_id(block_id):
+    for _ in range(2):
+        for link in links:
+            if link[0] == block_id:
+                links.remove(link)
+            if link[1] == block_id:
+                links.remove(link)
+
+
